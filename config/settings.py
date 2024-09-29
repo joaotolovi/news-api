94cb4644681086a375c17ad6a4ad56e23f8500b8
joaotolovi <joaotolove@gmail.com>
2024-09-29 18:51:08 -0300

    ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7s(piqzkjskz#05xb%3z!pzt9b(%xsxb4t^-o__urn$^o7idl)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
